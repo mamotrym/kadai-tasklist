ced40bb2ed9b227cd8f6fa707a38fdc7bcdcc9a0
mamo.m <m_12w@yahoo.co.jp>
2020-05-31 20:35:58 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,12 +10,25 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- 新規作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('signup.get', '新規ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                @if (Auth::check())
+                    {{-- 新規作成ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- ユーザ詳細ページへのリンク --}}
+                            <li class="dropdown-item"><a href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', '新規ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,37 +2,48 @@
 
 @section('content')
 
-    <h1 class=h3>タスク一覧</h1>
+    @if (Auth::check())
+    <h1 class=h3>{{ Auth::user()->name }}のタスク一覧</h1>
     
-    {{-- ユーザ登録ページへのリンク --}}
-    {!! link_to_route('signup.get', '新規ユーザ登録', [], ['class' => 'btn btn-md btn-primary']) !!}
-    
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    {{-- 詳細ページリンク --}}
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>ステータス</th>
+                        <th>タスク</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        {{-- 詳細ページリンク --}}
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        
+        @endif
+        
+        {{-- ページネーションへのリンク --}}
+        {{ $tasks->links() }}
+        
+        {{-- 新規作成ページリンク --}}
+        {!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'btn btn-sm btn-primary']) !!}
+
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1 class="h3 mb-4">タスクリストへようこそ</h1>
+                <p>タスクリストのご利用にはユーザ登録を行ってください。</p>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', '新規ユーザ登録', [], ['class' => 'btn btn-md btn-primary']) !!}
+            </div>
+        </div>
+
     @endif
     
-    {{-- ページネーションへのリンク --}}
-    {{ $tasks->links() }}
-    
-    {{-- 新規作成ページリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'btn btn-sm btn-primary']) !!}
-
 @endsection
